7842920f9341afe386b813cae21e3e82ebd9ffc2
NomanAhmed <officialnoman515@gmail.com>
2024-11-11 07:13:01 +0300

    basics of typescirpt

---
+++ Basics/index.js
@@ -0,0 +1,21 @@
+"use strict";
+//@ts-ignore
+let str = "Noman";
+console.log(str);
+// let text: string = "Hello";
+// let text1: string = "From Typescript";
+// let greeting: string = text + text1;
+// console.log(greeting);
+let fullName = "Noman Ahemd";
+let fullNameUpperCase = fullName.toUpperCase();
+let fullNameLowerCase = fullName.toLowerCase();
+let firstName = fullName.slice(0, 5);
+console.log("FirstName", firstName);
+//@ts-ignore
+//  Define the fucntion
+function sumTwo(a, b) {
+    return a + b;
+}
+console.log(sumTwo(5, 5));
+/// Create object
+let user;
---
+++ Basics/index.ts
@@ -0,0 +1,31 @@
+//@ts-ignore
+let str: string = "Noman";
+console.log(str);
+
+// let text: string = "Hello";
+// let text1: string = "From Typescript";
+
+// let greeting: string = text + text1;
+// console.log(greeting);
+
+let fullName: string = "Noman Ahemd";
+
+let fullNameUpperCase: string = fullName.toUpperCase();
+let fullNameLowerCase: string = fullName.toLowerCase();
+let firstName: string = fullName.slice(0, 5);
+console.log("FirstName", firstName);
+
+//@ts-ignore
+//  Define the fucntion
+function sumTwo(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sumTwo(5, 5));
+
+/// Create object
+let user: {
+  name: string;
+  age: number;
+  isAdult: boolean;
+};
